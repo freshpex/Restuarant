d58bf99a948e7e510428e4997c86ed7d214c19a6
Enoch Epekipolu <epekipoluenoch@gmail.com>
2025-03-10 13:31:50 +0100

    feat(PWA): enhance PWA support with updated manifest, improved security headers, and HTTPS redirection.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    https: process.env.NODE_ENV === 'production',
+    port: 5173
+  },
   plugins: [
     react(),
     VitePWA({
@@ -105,7 +109,8 @@ export default defineConfig({
             }
           }
         ],
-        navigateFallback: '/offline.html'
+        navigateFallback: '/offline.html',
+        force: true
       }
     })
   ],
